dc18bd6679445d41a2f41906625f492649802f30
alppacat <lorena.pratts@gmail.com>
2018-09-03 14:35:59 -0500

    Agrega función que lee archivos README y regresa un arreglo con los links de este, también se agrega color al texto

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const fileDir = require('path');
+const marked = require('marked');
+const path = process.argv[2];
+const log = console.log;
+const dir = fileDir.extname(path);
 
-const mdLinks = () => {
-  fs.readFile('./README.md', 'utf8', (err, content) => {
-    if (err) throw err;
-    console.log(content);
+
+const mdLinks = (path) => {
+  fs.readFile(path, 'utf8', (err, content) => {
+    if (dir !== '.md') {
+      console.log('Not an .md File', err);
+    } else {
+      parseMarked(content);
+    }
   });
 };
+
+const parseMarked = (content) => {
+  const toHtml = marked(content);
+  const regExp = /href="(.*?)"/g;
+  const links = toHtml.match(regExp);
+  links.forEach((urls) = (links) => {
+    log(chalk.bgGreen('Link: ') + chalk.magenta(links));
+  });
+
+};
+
+mdLinks(path);
 
 module.exports = {
   mdLinks,
 };
-
-mdLinks();
