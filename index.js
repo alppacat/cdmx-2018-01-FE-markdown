8443933d9f9b6ea3a22df7b092e8e604caa590c4
alppacat <lorena.pratts@gmail.com>
2018-09-03 20:25:32 -0500

    Elimina href= de los links proporcionados

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 const fileDir = require('path');
 const marked = require('marked');
+const figlet = require('figlet');
 const log = console.log;
-// const dir = fileDir.extname(path);
 
 const pathUndefined = () => {
   let path;
@@ -11,7 +11,13 @@ const pathUndefined = () => {
     path = process.argv[2];
     mdLinks(path);
   } else {
-    log('Escribe tu direccion');
+    figlet('MarkdownLinks', (err, data) => {
+      if (err) {
+        log('Something went wrong...');
+        console.dir(err);
+      }
+      log(chalk.magenta(data));
+    });
   }
 };
 
@@ -19,7 +25,7 @@ const mdLinks = (path) => {
   const dir = fileDir.extname(path);
   fs.readFile(path, 'utf8', (err, content) => {
     if (dir !== '.md') {
-      log('Not an .md File', err);
+      log(chalk.bgRed('Not an .md File', err));
     } else {
       parseMarked(content);
     }
@@ -29,13 +35,24 @@ const mdLinks = (path) => {
 const parseMarked = (content) => {
   const toHtml = marked(content);
   const regExp = /href="(.*?)"/g;
+  const regExpText = />[^<]*<[ ]*\/a[ ]*>/g;
   const links = toHtml.match(regExp);
+  const textLinks = toHtml.match(regExpText);
+  // let arr = [];
+
+  // textLinks.forEach((url) = (textLinks) => {
+  //   arr.push(textLinks);
+  // });
   links.forEach((urls) = (links) => {
-    log(chalk.bgGreen('Link: ') + chalk.magenta(links));
+    // arr.push(links)
+    log(chalk.bgGreen('Link: ') + chalk.magenta(links.split('href=')));
   });
+  // console.log(arr)
 };
 
 pathUndefined();
 module.exports = {
   mdLinks,
+  parseMarked,
+  pathUndefined
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,25 @@
+
 const { mdLinks} = require('../index');
+const {pathUndefined} = require('../index');
+const {parseMarked} = require('../index');
+const {chalk} = require('chalk');
+const {fileDir} = require('path');
+const {marked} = require('marked');
+const {figlet} = require('figlet');
 
 describe('Function mdLinks', () => {
-  test('Debería ser una función', () => {
-    expect(mdLinks('.md')).toBe(string);
+  test('It should be', () => {
+    expect(mdLinks('./READMELabo.md')).toBe();
+  });
+});
+describe('Path undefined Function', () => {
+  test('It should be...', () => {
+    expect(pathUndefined(process.argv[2]).toBeFalsy());
+  });
+});
+
+describe('Parse Marked function', () => {
+  test('It should be...', () => {
+    expect(parseMarked().toBe());
   });
 });
